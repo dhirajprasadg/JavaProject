9beac249a31fbf69a06063349d213e9f8ba916c4
Dhiraj Kumar <dhirajprasadg@gmail.com>
2016-12-09 00:48:24 +0530

    changes

--- JavatpointCoreJava/src/corejava/string/stringbuffer/StringBufferexample.java
+++ JavatpointCoreJava/src/corejava/string/stringbuffer/StringBufferexample.java
@@ -18,6 +18,25 @@ public class StringBufferexample {
 		System.out.println(" String Buffer replace value : " + sf1);
 		
 		System.out.println(" StringBuffer Capacity : " + sf1.capacity());
+		
+		System.out.println("-------------------------------------------------");
+		
+		String st = "This is apple ";
+		
+		StringBuffer sbf = new StringBuffer(st);
+		System.out.println(" Value of String in String Buffer : " + sbf );
+	   System.out.println(" sbf.reverse() : " +  sbf.reverse()); 
+	   System.out.println( " sbf.toString() : " + sbf.toString());
+	   sbf.reverse();
+	   String str =sbf.toString();
+	   System.out.println(" stringfrombuf : " + str);
+	   int in = str.indexOf("is");
+	   System.out.println(" str.indexOf(\"is\") : " +  in);
+		System.out.println(str.indexOf("ap"));
+	System.out.println(" str.substring(1) : " + str.substring(2));
+	System.out.println(" str.charAt(3) : " + str.charAt(3));
+	char[] ch = str.toCharArray();
+System.out.println(" str.toCharArray()" + ch);		
 	}
 
 }
